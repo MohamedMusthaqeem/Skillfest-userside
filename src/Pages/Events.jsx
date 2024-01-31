9ae61d0a290e7200aa4b5dc55b51f0b507a89f98
Mohamed-Musthaqeem <mohamedmusthaqeem.a@rbg.ai>
2024-01-31 13:26:32 +0530

    minor fixes1

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "../Components/Card";
 
 const Events = () => {
+  const [event, setEvent] = useState([]);
+  useEffect(() => {
+    const getEvents = async () => {
+      const res = await axios.get("http://localhost:5000/api/events");
+      const data = res.data;
+      if (res.status) {
+        setEvent(data);
+        console.log(data);
+      }
+    };
+    getEvents();
+  }, []);
   return (
-    <div>
-      <div>this is events</div>
+    <div className="min-h-screen">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {event && event.map((eve) => <Card key={eve._id} com={eve} />)}
+      </div>
     </div>
   );
 };
